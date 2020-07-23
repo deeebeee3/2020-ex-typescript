14a363d1c185d5373abadf7e7c77799b13a39f71
Deepak Bhari <deepak.bhari@lifts.to>
2020-07-23 13:38:19 +0100

    Handling component state

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 interface AppProps {
-    color?: string; //optional prop
+  color?: string; //optional prop
 }
 
-class App extends React.Component<AppProps> {
-    render() {
-        return (
-            <div>{this.props.color}</div>
-        )
-    }
+interface AppState {
+  counter: number;
 }
 
-ReactDOM.render(
-    <App color="red" />,
-    document.querySelector('#root')
-);
+class App extends React.Component<AppProps, AppState> {
+  //property method of initializing state object - override state property
+  //state = { counter : 0};
+
+  //constructor method of initializing state object - not overriding state property
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.color}
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App color="red" />, document.querySelector("#root"));
