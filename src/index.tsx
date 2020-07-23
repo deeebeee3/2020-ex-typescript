fff0195bf9d9f97315edf6693d8231e5b3b250c3
Deepak Bhari <deepak.bhari@lifts.to>
2020-07-23 15:03:02 +0100

    reinstall npm modules to fix missing react types issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import AppC from './components/AppC';
-import AppF from './components/AppF';
+import AppC from "./components/AppC";
+import AppF from "./components/AppF";
 
 ReactDOM.render(<AppF color="red" />, document.querySelector("#root"));
